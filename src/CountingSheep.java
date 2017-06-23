c4944139db857efc2db33e839ad543902928a96a
Young Park <young_park@brown.edu>
2017-06-23 21:00:16 +0900

    Finished first attempt at an algorithm. Testing when n = 2.

--- src/CountingSheep.java
+++ src/CountingSheep.java
@@ -14,6 +14,54 @@ public class CountingSheep {
         }
         this.n = n;
         this.increment = 1; // multiplier for n
+        checkCount();
     }
 
+    private void checkCount() {
+        for (int p = 0; p < 13; p++) {
+            String temp = String.valueOf(this.n);
+            for (int i = 0; i < temp.length(); i++) {
+                int digit = Integer.parseInt(temp.substring(i, i+1));
+                if (digit == 0) {
+                    this.isCounted[0] = true;
+                } else if (digit == 1) {
+                    this.isCounted[1] = true;
+                } else if (digit == 2) {
+                    this.isCounted[2] = true;
+                } else if (digit == 3) {
+                    this.isCounted[3] = true;
+                } else if (digit == 4) {
+                    this.isCounted[4] = true;
+                } else if (digit == 5) {
+                    this.isCounted[5] = true;
+                } else if (digit == 6) {
+                    this.isCounted[6] = true;
+                } else if (digit == 7) {
+                    this.isCounted[7] = true;
+                } else if (digit == 8) {
+                    this.isCounted[8] = true;
+                } else if (digit == 9) {
+                    this.isCounted[9] = true;
+                }
+            }
+            if (isAsleep()) {
+                break;
+            }
+            this.increment++;
+            this.n = this.increment * this.n;
+        }
+    }
+
+    private boolean isAsleep() {
+        for (int i = 0; i < this.isCounted.length; i++) {
+            if (!this.isCounted[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        CountingSheep c = new CountingSheep(2);
+    }
 }
